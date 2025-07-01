1d2f79cfe1577525505cd62a5aba75f7e59c8698
reytler <reytlercompare@hotmail.com>
2025-07-01 09:07:41 -0300

    Initial commit

---
+++ api/src/index.ts
@@ -0,0 +1,21 @@
+import express , { Request, Response } from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+const app = express();
+const server = createServer(app)
+const io = new Server(server)
+const port = process.env.PORT || 3001
+
+app.use(express.json());
+
+app.get("/", (req: Request, res:Response)=>{
+    res.send("Hello Express")
+});
+
+io.on('connection',(socket)=>{
+    console.log('A host conected');
+});
+
+server.listen(port,()=>{
+    console.log(`Server is running at http://localhost:${port}`)
+});
