6d7193cb029f07949b11650124f721a4a816b3ac
reytler <reytlercompare@hotmail.com>
2025-07-09 17:28:13 -0300

    FEAT: Adding message validation and sending with shift+Enter

--- front/src/Components/ChatComponent/index.tsx
+++ front/src/Components/ChatComponent/index.tsx
@@ -1,11 +1,14 @@
 import { UserDTO } from "@shared/dtos/UserDTO"
 import { MessageDTO } from "@shared/dtos/MessageDTO"
-import { NotificationType, notify, NotifyHeader } from "../Notification"
+import { NotifyHeader } from "../Notification"
 import { Button } from "../ui/button"
 import { ScrollArea } from "../ui/scroll-area"
 import { Separator } from "../ui/separator"
 import { Textarea } from "../ui/textarea"
-import { useState } from "react"
+import z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
 
 export type THomeProps = {
     connectedUsers: UserDTO[]
@@ -15,8 +18,31 @@ export type THomeProps = {
     messages: MessageDTO[]
     handleSendMessage: (message: MessageDTO)=>void
 }
+
+const schema = z.object({
+  content: z.string().min(2, 'Digite uma mensagem para enviar'),
+})
+
+type FormData = z.infer<typeof schema>
+
 export function ChatComponent({userName, idConnection, handleLogout,messages, connectedUsers,handleSendMessage}: THomeProps){
-    const [content,setContent] = useState<string>("");
+
+    const form = useForm<FormData>({
+        resolver: zodResolver(schema),
+        defaultValues: {
+            content:''
+        },
+    })
+
+    function onSubmit(data:FormData){
+        const newmessage: MessageDTO = {
+            content: data.content,
+            idConnection: idConnection,
+            userName: userName
+        }
+        handleSendMessage(newmessage)
+    }
+
      return(
         <div className="h-screen flex flex-col">
             <header className="p-4 shadow z-10 flex gap-2">
@@ -35,31 +61,43 @@ export function ChatComponent({userName, idConnection, handleLogout,messages, co
                     </ScrollArea>
 
                     <Separator />
-
-                    <form
-                        className="flex items-center gap-2 p-4 border-t bg-background sticky bottom-0"
-                        onSubmit={(event) => {
-                            event.preventDefault();
-                            if(content.trim() === ""){
-                                notify("Digite algo antes de enviar!",NotificationType.ERROR)
-                                return
-                            }
-                            const newmessage: MessageDTO = {
-                                content: content,
-                                idConnection: idConnection,
-                                userName: userName
-                            }
-                            handleSendMessage(newmessage)
-                            setContent("")
-                        }}
-                    >
-                        <Textarea
-                            placeholder="Digite sua mensagem..."
-                            onChange={(event) =>setContent(event.target.value)}
-                            value={content}
-                        />
-                        <Button type="submit">Enviar</Button>
-                    </form>
+                    <Form {...form}>
+                        <form
+                            className="flex items-center gap-2 p-4 border-t bg-background sticky bottom-0"
+                            onSubmit={form.handleSubmit((data)=>{
+                                onSubmit(data)
+                                form.resetField('content')
+                            })}
+                        >
+                            <FormField
+                                control={form.control}
+                                name="content"
+                                render={({field})=>(
+                                    <FormItem className="flex flex-col items-center mt-5">
+                                        <FormControl>
+                                            <Textarea
+                                                placeholder="Digite sua mensagem..."
+                                                {...field}
+                                                onKeyDown={(e)=>{
+                                                    if(e.key === "Enter" && e.shiftKey){
+                                                        e.preventDefault()
+                                                        form.handleSubmit((data)=>{
+                                                            onSubmit(data)
+                                                            form.resetField('content')
+                                                        })()
+                                                    }
+                                                }}
+                                                title="shift+Enter para enviar"                                
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            
+                            <Button type="submit">Enviar</Button>
+                        </form>                        
+                    </Form>
                 </div>
                 <aside className="w-60 border-l p-4 bg-muted/40">
                     <h2 className="font-semibold mb-2">Online</h2>
